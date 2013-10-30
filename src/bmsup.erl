12e4a84fed8921c6928749feabd1cb8f5feeb2a6
Leonhard Markert <curiousleo@ymail.com>
2013-10-30 20:11:37 +0000

    erlang experiments

--- src/bmsup.erl
+++ src/bmsup.erl
@@ -1,15 +1,15 @@
 -module(bmsup).
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/1]).
 -export([init/1]).
 
-start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Args) -> supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
 
-init([]) ->
-    {ok, {{one_for_one, 5, 60},
+init([Func, N]) ->
+    {ok, {{simple_one_for_one, 5, 60},
           [{bmworker_id,
-            {bmworker, start_link, []},
+            {bmworker, start_link, [Func, N]},
             transient,
             brutal_kill,
             worker,
--- src/bmworker.erl
+++ src/bmworker.erl
@@ -2,8 +2,8 @@
 -behaviour(gen_server).
 
 -export([repeat_rpc/2]).
--export([latency/3]).
--export([start_link/0]).
+-export([latency/1]).
+-export([start_link/2]).
 -export([init/1,
          handle_call/3, handle_cast/2, handle_info/2,
          terminate/2,
@@ -11,21 +11,27 @@
 
 -define(TIMEOUT, 3000).
 
-latency(Pid, F, N) ->
-    gen_server:call(Pid, {latency, F, N}).
+latency(Pid) ->
+    gen_server:call(Pid, latency).
 
-repeat_rpc(_F, 0) -> ok;
+repeat_rpc(_Func, 0) -> ok;
 repeat_rpc({Node, M, F, A}, N) ->
     rpc:call(Node, M, F, A, ?TIMEOUT),
     repeat_rpc({Node, M, F, A}, N-1).
 
-start_link() -> gen_server:start_link(?MODULE, [], []).
+start_link(Func, N) -> gen_server:start_link(?MODULE, [Func, N], []).
 
-init([]) -> {ok, []}.
+init([Func, N]) -> {ok, {Func, N}}.
 
-handle_call({latency, F, N}, _From, _State) ->
-    {Latency, ok} = timer:tc(?MODULE, repeat_rpc, [F, N]),
-    {reply, Latency / N, []}.
+% init([Func={Node, M, F, A}, N]) ->
+%     case rpc:call(Node, M, F, A, ?TIMEOUT) of
+%         {badrpc, Reason} -> {stop, Reason};
+%         _ -> {ok, {Func, N}}
+%     end.
+
+handle_call(latency, _From, {Func, N}) ->
+    {Latency, ok} = timer:tc(?MODULE, repeat_rpc, [Func, N]),
+    {reply, Latency / N, {Func, N}}.
 
 handle_cast(_, _) -> erlang:error("handle_cast").
 
