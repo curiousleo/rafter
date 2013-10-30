e5429bcf907092708c7765469bfeaf67c5487cb0
Leonhard Markert <curiousleo@ymail.com>
2013-10-30 21:43:08 +0000

    bmsup: not quite

--- src/bmsup.erl
+++ src/bmsup.erl
@@ -4,12 +4,30 @@
 -export([start_link/1]).
 -export([init/1]).
 
-start_link(Args) -> supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
+start_link(M, [Func, N]) ->
+    {ok, Super} = supervisor:start_link({local, ?MODULE}, ?MODULE, Args),
+    Children = start_children(Super, M, [Func, N, self()], []),
+    % Children = [ C || _N <- lists:seq(1, M), {ok, C} <- supervisor:start_child(Super, Args) ],
+    receive
+        start -> lists:foreach(Children, fun(C) -> C ! latency end),
+                 collect_results(Super, M, [])
+    end.
 
-init([Func, N]) ->
+collect_results(Super, 0, Results) -> Super ! {results, Results};
+collect_results(Super, M, Results) ->
+    receive
+        {latency, Latency} -> collect_results(Super, M-1, [Latency|Results])
+    end.
+
+start_children(_Super, 0, _Args, Children) -> Children;
+start_children(Super, M, Args, Children) ->
+    {ok, Child} = supervisor:start_child(Super, Args),
+    start_children(Super, M-1, Args, [Child|Children]).
+
+init([]) ->
     {ok, {{simple_one_for_one, 5, 60},
           [{bmworker_id,
-            {bmworker, start_link, [Func, N]},
+            {bmworker, start_link, []},
             transient,
             brutal_kill,
             worker,
