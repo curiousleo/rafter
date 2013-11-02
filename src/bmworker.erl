b32697e005ea499b9a226f850926e4ef61205e9d
Leonhard Markert <curiousleo@ymail.com>
2013-11-02 21:13:57 +0000

    bmworker: supervisor behaviour

--- src/bmworker.erl
+++ src/bmworker.erl
@@ -1,5 +1,7 @@
 -module(bmworker).
+-behaviour(supervisor).
 
+-export([init/1]).
 -export([time_rpc/2]).
 
 -define(TIMEOUT, 3000).
@@ -12,3 +14,15 @@ repeat_rpc({Node, M, F, A}, N) ->
 time_rpc(Func, N) ->
     {Latency, ok} = timer:tc(?MODULE, repeat_rpc, [Func, N]),
     Latency / N.
+
+init([Func, N]) ->
+    {ok, {{simple_one_for_one, 5, 60},
+          [{bmworker_id,
+            {?MODULE, time_rpc, [Func, N]},
+            transient,
+            brutal_kill,
+            worker,
+            [bmworker]}
+          ]
+         }
+    }.
