05d06a94eca6f810ff6c02ee5ea13ea528de2414
Leonhard Markert <curiousleo@ymail.com>
2013-11-03 16:30:46 +0000

    use Conc

--- src/bmsup.erl
+++ src/bmsup.erl
@@ -9,17 +9,17 @@
 init(Distr) ->
     {ok, waiting, Distr}.
 
-waiting({start, {Func, N}}, Distr) ->
-    Sup = supervisor:start_link(bmworker, {Func, N, self()}),
-    lists:foreach(fun(_N) -> supervisor:start_child(Sup, []) end, lists:seq(1, N)),
-    {next_state, running, {[], N, Sup, Distr}}.
+waiting({start, {Func, Conc, Times}}, Distr) ->
+    Sup = supervisor:start_link(bmworker, {Func, Times, self()}),
+    lists:foreach(fun(_N) -> supervisor:start_child(Sup, []) end, lists:seq(1, Conc)),
+    {next_state, running, {[], Conc, Sup, Distr}}.
 
 running({done, Latency}, {Latencies, 0, Sup, Distr}) ->
     Distr ! {self(), done, [Latency|Latencies]},
     {next_state, waiting, Sup, Distr};
 
-running({done, Latency}, {Latencies, N, Sup, Distr}) ->
-    {next_state, running, {[Latency|Latencies], N-1, Sup, Distr}}.
+running({done, Latency}, {Latencies, Conc, Sup, Distr}) ->
+    {next_state, running, {[Latency|Latencies], Conc-1, Sup, Distr}}.
 
 terminate(_Reason, waiting, _StateData) ->
     ok;
