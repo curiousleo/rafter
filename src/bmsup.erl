39266d73783253d2b533535e89772734a035a44a
Leonhard Markert <curiousleo@ymail.com>
2013-11-03 09:47:53 +0000

    bmsup: gen_fsm

--- src/bmsup.erl
+++ src/bmsup.erl
@@ -1,42 +1,19 @@
 -module(bmsup).
--behaviour(supervisor).
+-behaviour(gen_fsm).
 
--export([start_link/1]).
 -export([init/1]).
 
-start_link([]) ->
-    {ok, Super} = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
-    erlang:register(bmsup_p, self()),
-    receive
-        {Distr, {init, {Conc, Func, Times}}} ->
-            Children = start_children(Super, Conc, [Func, Times, self()], [])
-            % Children = [ C || _N <- lists:seq(1, Conc), {ok, C} <- supervisor:start_child(Super, Args) ]
-    end,
-    Distr ! ready,
-    receive
-        start -> lists:foreach(fun(C) -> C ! latency end, Children)
-    end,
-    collect_results(Distr, Conc, []).
+init(Distr) ->
+    {ok, waiting, Distr}.
 
-collect_results(Distr, 0, Results) -> Distr ! {results, Results};
-collect_results(Distr, Conc, Results) ->
-    receive
-        {latency, Latency} -> collect_results(Distr, Conc-1, [Latency|Results])
-    end.
+waiting({start, {Func, N}}, Distr) ->
+    Sup = supervisor:start_link(bmworker, {Func, N}),
+    lists:foreach(fun(_N) -> supervisor:start_child(Sup, []) end, seq(1, N)),
+    {next_state, running, {[], N, Distr}}.
 
-start_children(_Super, 0, _Args, Children) -> Children;
-start_children(Super, Conc, Args, Children) ->
-    {ok, Child} = supervisor:start_child(Super, Args),
-    start_children(Super, Conc-1, Args, [Child|Children]).
+running({done, Latency}, {Latencies, 0, Distr}) ->
+    Distr ! {self(), done, [Latency|Latencies]},
+    {next_state, waiting, Distr};
 
-init([]) ->
-    {ok, {{simple_one_for_one, 5, 60},
-          [{bmworker_id,
-            {bmworker, start_link, []},
-            transient,
-            brutal_kill,
-            worker,
-            [bmworker]}
-          ]
-         }
-    }.
+running({done, Latency}, {Latencies, N, Distr}) ->
+    {next_state, running, {[Latency|Latencies], N-1}}.
