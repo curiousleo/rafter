d6daa9a4fcf99967d1756711583825ce3794452b
Leonhard Markert <curiousleo@ymail.com>
2013-11-17 23:28:41 +0000

    callback

--- src/bmdistr.erl
+++ src/bmdistr.erl
@@ -2,6 +2,7 @@
 -behaviour(gen_fsm).
 
 -export([benchmark/4]).
+-export([callback/3]).
 -export([init/1, terminate/3,
          handle_event/3, handle_sync_event/4,
          handle_info/3, code_change/4,
@@ -15,7 +16,7 @@
     running=0 :: non_neg_integer()
 }).
 
-benchmark(Func, Nodes, Conc, Times) ->
+benchmark(Nodes, Func, Conc, Times) ->
     {ok, Distr} = gen_fsm:start_link(?MODULE, Nodes, []),
     gen_fsm:send_event(Distr, {start, {Func, Conc, Times}}).
 
@@ -26,25 +27,33 @@ waiting({add_node, Node}, State=#state{nodes=Nodes}) ->
     {next_state, waiting, State#state{nodes=[Node|Nodes]}};
 
 waiting({start, {Func, Conc, Times}}, State=#state{nodes=Nodes}) ->
-    Msg = {start, {Func, Conc, Times, self()}},
-    lists:foreach(fun(Node) ->  {bmsup_p, Node} ! Msg end, Nodes),
+    Msg = {start, {Func, Conc, Times}},
+    Args = [Msg, self()],
+    lists:foreach(fun(Node) -> spawn(?MODULE, callback, [Node|Args]) end, Nodes),
     NewState = State#state{conc=Conc, times=Times, running=length(Nodes)},
     {next_state, running, NewState}.
 
-running({done, Latencies}, State=#state{latencies=AvgLatencies, running=0}) ->
+callback(Node, Msg, Distr) ->
+    {done, Latencies} = gen_server:call({bmsup_p, Node}, Msg),
+    io:format("callback received ~w~n", [{done, Latencies}]),
+    Distr ! {done, Latencies}.
+
+running({done, Latencies}, State=#state{latencies=AvgLatencies, running=1}) ->
+    io:format("running received ~w (last one)~n", [{done, Latencies}]),
     Avg = lists:sum(Latencies) / State#state.times,
-    lists:foreach(fun(L) -> io:format("Latency: ~w~n", L) end, [Avg|AvgLatencies]),
-    {next_state, waiting, State}; % ??
+    {stop, normal, State#state{latencies=[Avg|AvgLatencies]}}; % ??
 
 running({done, Latencies}, State=#state{latencies=AvgLatencies, running=R}) ->
+    io:format("running received ~w (~w to go)~n", [{done, Latencies}, R]),
     Avg = lists:sum(Latencies) / State#state.times,
     {next_state, running, State#state{latencies=[Avg|AvgLatencies], running=R-1}}.
 
 terminate(_Reason, waiting, _StateData) ->
     ok;
 
-terminate(_Reason, running, #state{nodes=_Nodes}) ->
+terminate(normal, running, #state{latencies=Latencies}) ->
     % TODO: do something
+    io:format("Latencies: ~w~n", [Latencies]),
     ok.
 
 handle_event(Event, _StateName, _StateData) ->
@@ -54,6 +63,7 @@ handle_sync_event(Event, _From, _StateName, _StateData) ->
     {stop, "unknown synchronous event occurred", Event}.
 
 handle_info(Info, _StateName, _StateData) ->
+    io:format("unknown event: ~w~n", [Info]),
     {stop, "unknown event occurred", Info}.
 
 code_change(_OldVsn, StateName, StateData, _Extra) ->
