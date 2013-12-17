77799f9c2c75fb24253a42a2b81bb9d9757380c6
Leonhard Markert <curiousleo@ymail.com>
2013-12-17 21:29:54 +0000

    updated paths

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-erlc -pa lib/eqc-1.0.1/ebin -o ebin src/*.erl test/*.erl
-erl -pa ebin -pa lib/eqc-1.0.1/ebin -eval 'eqc:module(deque_eqc).' -s init stop -noshell
+erlc -pa deps/eqc/ebin -I deps -o ebin src/*.erl test/*.erl
+erl -pa ebin -pa deps/eqc/ebin -I deps -eval 'eqc:module(deque_eqc).' -s init stop -noshell
