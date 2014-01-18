fae17653b26fbd651992092050af693a3bbd4878
Leonhard Markert <curiousleo@ymail.com>
2014-01-18 00:35:10 +0000

    parsing and calling rafter:op / rafter:read_op

--- src/rafter_memcached_sock.erl
+++ src/rafter_memcached_sock.erl
@@ -1,5 +1,7 @@
 -module(rafter_memcached_sock).
 
+-include("rafter.hrl").
+
 -behaviour(gen_server).
 
 %% api
@@ -9,22 +11,35 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3,
          terminate/2]).
 
--type normal_cmd() ::
-   {set | add | replace | append | prepend,   % command
-    binary(),                                 % key
-    binary(),                                 % flags
-    non_neg_integer(),                        % exptime
-    non_neg_integer(),                        % bytes
-    boolean()}.                               % noreply?
--type cas_cmd() ::
-   {cas,
-    binary(),                                 % key
-    binary(),                                 % flags
-    non_neg_integer(),                        % exptime
-    non_neg_integer(),                        % bytes
-    binary(),                                 % cas unique
-    boolean()}.                               % noreply?
--type cmd() :: normal_cmd() | cas_cmd().
+-record(state, {
+          peer :: peer(),
+          socket :: port(),
+          next = command :: command | data,
+          command :: cmd(),
+          data = <<>> :: binary(),
+          bytes = 0 :: non_neg_integer()
+         }).
+
+-record(storage_cmd, {
+          command :: set | add | replace | append | prepend,
+          key :: binary(),
+          flags :: binary(),
+          exptime :: non_neg_integer(),
+          bytes :: non_neg_integer(),
+          noreply = false :: boolean()
+         }).
+-record(cas_cmd, {
+          key :: binary(),
+          flags :: binary(),
+          exptime :: non_neg_integer(),
+          bytes :: non_neg_integer(),
+          cas_unique :: binary(),
+          noreply = false :: boolean()
+         }).
+-record(retrieval_cmd, {
+          keys :: [binary()]
+         }).
+-type cmd() :: #storage_cmd{} | #cas_cmd{} | #retrieval_cmd{}.
 
 %%
 %% api
@@ -39,20 +54,56 @@ start_link(ListenSocket, Peer) ->
 
 init([ListenSocket, Peer]) ->
     gen_server:cast(self(), accept),
-    {ok, {ListenSocket, Peer}}.
+    {ok, #state{socket = ListenSocket, peer = Peer, next = command}}.
 
 handle_call(_Request, _From, State) ->
     {noreply, State}.
 
-handle_cast(accept, {ListenSocket, Peer}) ->
+handle_cast(accept, S = #state{socket = ListenSocket, peer = Peer}) ->
     {ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
     rafter_memcached_sup:start_socket(Peer),
     send(AcceptSocket, "WHATS UP", []),
-    {noreply, {AcceptSocket, Peer}}.
+    {noreply, S#state{socket = AcceptSocket}}.
 
-handle_info({tcp, Socket, Str}, {AcceptSocket, Peer}) ->
-    send(Socket, "~p", [parse_command(Str)]),
-    {noreply, {AcceptSocket, Peer}};
+handle_info({tcp, Socket, Str}, S = #state{peer = Peer, next = command}) ->
+    case parse_cmd(Str) of
+        {ok, Cmd = #retrieval_cmd{}} ->
+            Result = execute_read_cmd(Peer, Cmd),
+            send(Socket, "~p", [Result]),
+            {noreply, S};
+        {ok, Cmd} ->
+            send(Socket, "Waiting for data", []),
+            {noreply, S#state{next = data, command = Cmd, bytes = bytes(Cmd)}};
+        error ->
+            send(Socket, "ERROR in 1st clause", []),
+            {noreply, S}
+    end;
+handle_info({tcp, Socket, Str},
+            S = #state{peer = Peer, next = data, data = Data, command = Cmd,
+                       bytes = Bytes})
+  when byte_size(Str) =:= Bytes + 2 ->
+    case Str of
+        <<NewData:Bytes/binary, "\r\n">> ->
+            Result = execute_write_cmd(Peer, Cmd,
+                                       <<Data/binary,NewData/binary>>),
+            send(Socket, "~p", [Result]);
+        _ ->
+            send(Socket, "ERROR in 2nd clause", [])
+    end,
+    {noreply, S#state{next = command, data = <<>>, bytes = 0}};
+handle_info({tcp, Socket, Str},
+            S = #state{next = data, data = Data, bytes = Bytes})
+  when byte_size(Str) < Bytes + 2 ->
+    send(Socket, "Waiting", []),
+    {noreply, S#state{data = <<Data/binary,Str/binary>>,
+                      bytes = Bytes - byte_size(Str)}};
+handle_info({tcp, Socket, _Str}, S = #state{next = data, bytes = Bytes})
+  when Bytes < 1 ->
+    send(Socket, "ERROR in 4th clause", []),
+    {noreply, S#state{data = <<>>, bytes = 0, next = command}};
+handle_info({tcp, Socket, _Str}, S = #state{next = data}) ->
+    send(Socket, "ERROR in 5th clause", []),
+    {noreply, S#state{data = <<>>, bytes = 0, next = command}};
 handle_info({tcp_closed, _Socket}, S) ->
     {stop, normal, S};
 handle_info({tcp_error, _Socket}, S) ->
@@ -68,8 +119,8 @@ terminate(_Reason, _State) ->
 %% helper functions
 %%
 
--spec parse_command(binary()) -> {ok, cmd()} | error.
-parse_command(CmdBin) ->
+-spec parse_cmd(binary()) -> {ok, cmd()} | error.
+parse_cmd(CmdBin) ->
     StrippedCmdBin = binary:part(CmdBin, {0, byte_size(CmdBin) - 2}),
     case binary:split(StrippedCmdBin, [<<" ">>], [global]) of
         [<<"set">> | Args] ->       parse_args(set, Args);
@@ -78,31 +129,57 @@ parse_command(CmdBin) ->
         [<<"append">> | Args] ->    parse_args(append, Args);
         [<<"prepend">> | Args] ->   parse_args(prepend, Args);
         [<<"cas">> | Args] ->       parse_args(cas, Args);
+        [<<"get">> | Keys] ->       parse_args(get, Keys);
+        [<<"gets">> | Keys] ->      parse_args(get, Keys);
         _ ->                        error
     end.
 
 parse_args(cas, [Key, Flags, Exp, Bytes, CasUnique]) ->
-    {ok,
-     {cas, Key, Flags, binary_to_integer(Exp),
-      binary_to_integer(Bytes), CasUnique, false}
+    {ok, #cas_cmd{key = Key, flags = Flags, exptime = binary_to_integer(Exp),
+                  bytes = binary_to_integer(Bytes), cas_unique = CasUnique,
+                  noreply = false}
     };
 parse_args(cas, [Key, Flags, Exp, Bytes, CasUnique, <<"noreply">>]) ->
-    {ok,
-     {cas, Key, Flags, binary_to_integer(Exp),
-      binary_to_integer(Bytes), CasUnique, true}
+    {ok, #cas_cmd{key = Key, flags = Flags, exptime = binary_to_integer(Exp),
+                  bytes = binary_to_integer(Bytes), cas_unique = CasUnique,
+                  noreply = true}
     };
+parse_args(get, Keys) ->
+    {ok, #retrieval_cmd{keys = Keys}};
 parse_args(Cmd, [Key, Flags, Exp, Bytes]) ->
-    {ok,
-     {Cmd, Key, Flags, binary_to_integer(Exp),
-      binary_to_integer(Bytes), false}
+    {ok, #storage_cmd{command = Cmd, key = Key, flags = Flags,
+                      exptime = binary_to_integer(Exp),
+                      bytes = binary_to_integer(Bytes), noreply = false}
     };
 parse_args(Cmd, [Key, Flags, Exp, Bytes, <<"noreply">>]) ->
-    {ok,
-     {Cmd, Key, Flags, binary_to_integer(Exp),
-      binary_to_integer(Bytes), true}
+    {ok, #storage_cmd{command = Cmd, key = Key, flags = Flags,
+                      exptime = binary_to_integer(Exp),
+                      bytes = binary_to_integer(Bytes), noreply = false}
     };
 parse_args(_, _) ->
     error.
+
+-spec execute_read_cmd(peer(), #retrieval_cmd{}) ->
+    {ok, term()} | {error, term()}.
+execute_read_cmd(Peer, #retrieval_cmd{keys = Keys}) ->
+    rafter:read_op(Peer, {get, Keys}).
+
+-spec execute_write_cmd(peer(), #cas_cmd{} | #storage_cmd{}, binary()) ->
+        {ok, term()} | {error, term()}.
+execute_write_cmd(Peer, #storage_cmd{command = Cmd, key = Key,
+                                     flags = Flags, exptime = Exp},
+                  Value) ->
+    rafter:op(Peer, {Cmd, Key, Value, Flags, Exp});
+execute_write_cmd(Peer, #cas_cmd{key = Key, flags = Flags,
+                                 cas_unique = CasUnique, exptime = Exp},
+                  Value) ->
+    rafter:op(Peer, {cas, Key, Value, Flags, Exp, CasUnique}).
+
+-spec bytes(#storage_cmd{} | #cas_cmd{}) -> non_neg_integer().
+bytes(#storage_cmd{bytes = Bytes}) ->
+    Bytes;
+bytes(#cas_cmd{bytes = Bytes}) ->
+    Bytes.
 
 send(Socket, Str, Args) ->
     ok = gen_tcp:send(Socket, io_lib:format(Str ++ "~n", Args)),
