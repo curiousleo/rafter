b44b18ad7267f21543b5feaf6a7489a096edface
Leonhard Markert <curiousleo@ymail.com>
2014-04-26 20:59:22 +0100

    Revert "failed and restarted logging"
    This reverts commit 99a40e6ca8859d471dc4aefb25f93bdbb36c0dcb.

--- src/rafter_ttc_log.erl
+++ src/rafter_ttc_log.erl
@@ -31,13 +31,7 @@ handle_info({log_read, T},
     io:format(F, "~p,~p,Read,~p~n", [E, C, T]),
     {noreply, S};
 handle_info({set_experiment, {C, E}}, S) ->
-    {noreply, S#state{cluster_size = C, experiment = E}};
-handle_info(failed, S = #state{logfile = F}) ->
-    io:format(F, "FAILED", []),
-    S;
-handle_info(restarted, S = #state{logfile = F}) ->
-    io:format(F, "RESTARTED", []),
-    S.
+    {noreply, S#state{cluster_size = C, experiment = E}}.
 
 handle_call(_Request, _From, State) ->
     {stop, handle_call_called, State}.
